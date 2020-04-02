ca1bedb23e78fe60632e2becad786c6c0d0d9b3e
KarlyLamm <61996747+KarlyLamm@users.noreply.github.com>
2020-04-02 01:26:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# a_random_quote_generator-v1--2-
+ This project generates random quotes with the use of HTML, CSS, and JavaScript
---
+++ js/script.js
@@ -0,0 +1,88 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes=[
+  {
+    'quote' :'"Talk is cheap. Show me the code."' ,
+    'source':"Linus Torvalds",
+    'citation':'goodreads',
+    'year':2000
+  },
+  {
+    'quote':"People don't care about what you say, they care about what you build",
+    'source':"Mark Zuckerberg",
+    'citation':"Wired News",
+    'year':2014
+  },
+  {
+    'quote':"Programs must be written for people to read, and only incidentally for machines to execute.",
+    'source':"Harold Abelson",
+    'citation':'goodreads',
+    'year':1984
+  },
+  {
+    'quote':"You might not think that programmers are artists, but programming is an extremely creative profession. It's logic-based creativity.",
+    'source':"John Romero",
+    'citation':'Brainy Quote',
+    'year':2009
+  },
+  {
+    'quote':"Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    'source':"Patrick McKenzie",
+    'citation':"Twitter",
+    'year':2016
+  },
+
+]
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+function getRandomQuote(){
+let ranNumber=Math.floor(Math.random()*quotes.length);
+let randomQuote=quotes[ranNumber];
+
+return randomQuote;
+}
+
+
+/***
+ * `printQuote` function
+***/
+function printQuote(){
+let shownQuote=getRandomQuote()
+let html='<p class="quotes">'+shownQuote.quote+'</p>'
+html+='<p class="source">'+shownQuote.source
+
+
+html+='<span class="citation">'+shownQuote.citation+'</span>'
+
+
+
+html+='<span class="year">'+shownQuote.year+'</span>'
+
+  
+'</p>'
+
+document.getElementById('quote-box').innerHTML = html;
+
+}
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
